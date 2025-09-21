2525bc328d6567d5c5e0308e8ce80f3816945ea4
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2025-09-21 16:05:41 +0500

    Added gcmh and path variables

---
+++ early-init.el
@@ -0,0 +1,4 @@
+(setenv "SHELL" "/usr/bin/zsh")
+(add-to-list 'exec-path "~/.nvm/versions/node/v24.7.0/bin")
+(add-to-list 'exec-path "~/.local/bin")
+(add-to-list 'exec-path "~/.local/custom")
---
+++ lisp/init-gcmh.el
@@ -0,0 +1,29 @@
+;;; init-gcmh.el --- Garbage Collector Magic Hcak -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(defvar my-last-gc-time (current-time)
+  "Time when the last garbage collection finished.")
+
+(add-hook 'post-gc-hook
+          (lambda ()
+            (setq my-last-gc-time (current-time))))
+
+(defun my/time-since-last-gc ()
+  "Show and return seconds since the last garbage collection."
+  (interactive)
+  (let ((secs (float-time (time-subtract (current-time) my-last-gc-time))))
+    (when (called-interactively-p 'any)
+      (message "Time since last GC: %.2f seconds" secs))
+    secs))
+
+(use-package gcmh
+  :ensure t
+  :config
+  (setq gcmh-idle-delay 5)
+  (gcmh-mode 1)
+  )
+
+(provide 'init-gcmh)
+
+;;; init-gcmh.el ends here

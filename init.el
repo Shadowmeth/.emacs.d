dd4dd0655f90ec78c397d98eb7d146ec4b51c95d
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2025-09-24 02:22:24 +0500

    Clean up

--- init.el
+++ init.el
@@ -20,17 +20,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(doom-modeline exec-path-from-shell expand-region gcmh magit
-		   multiple-cursors rainbow-delimiters zzz-to-char)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )

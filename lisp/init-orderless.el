0147c6df0ede9c2b24ce1f02844749974314a508
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2025-09-25 23:20:24 +0500

    Changed orderless matching styles (not using flex anymore)

--- lisp/init-orderless.el
+++ lisp/init-orderless.el
@@ -7,7 +7,10 @@
   :custom
   ;; (orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch))
   ;; (orderless-component-separator #'orderless-escapable-split-on-space)
-  (orderless-matching-styles '(orderless-flex orderless-literal orderless-regexp))
+
+  ;; Use orderless-flex is a little to lax for in buffer auto completion
+  ;; (orderless-matching-styles '(orderless-flex orderless-literal orderless-regexp))
+  (orderless-matching-styles '(orderless-literal orderless-regexp))
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles partial-completion))))
   (completion-category-defaults nil) ;; Disable defaults, use our settings

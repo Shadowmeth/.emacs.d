8274294d7e2355a113d792b4d9c547a2856222aa
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2025-10-14 18:29:45 +0500

    Fixed syntax error in init-flycheck.el

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -6,7 +6,7 @@
   :ensure t
   :config
   (with-eval-after-load 'rust-mode
-    add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
   )
 
 (use-package flycheck

0d821ef02cb23123269ebe9e40538e48a1a0f5f0
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2026-02-08 19:55:24 +0500

    Keybind setup for dashboard

--- lisp/init-dashboard.el
+++ lisp/init-dashboard.el
@@ -10,6 +10,19 @@
   (setq initial-buffer-choice 'dashboard-open) ;; Show dashboard in frames with emacsclient -s
   ;; all-the-icons and nerd-icons may require a refresh in order to be displayed correctly
   (add-hook 'server-after-make-frame-hook 'dashboard-open)
+  
+(setq dashboard-items '((recents . 5)
+                           (bookmarks . 5)
+                           (projects . 5)
+                           (agenda . 5)))
+  
+  (setq dashboard-item-shortcuts '((recents . "r")
+                                    (bookmarks . "m")
+                                    (projects . "p")
+                                    (agenda . "a")
+                                    (registers . "e")))
+  (setq dashboard-display-icons-p t) ; display icons on both GUI and terminal
+  (setq dashboard-icon-type 'nerd-icons) ; use `nerd-icons' package
   )
 
 (provide 'init-dashboard)

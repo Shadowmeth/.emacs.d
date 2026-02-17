1a10454a0b611c0d4ceee0bc4f62734d751e731f
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2026-02-17 16:23:47 +0500

    Added surround and ligatures for more modes

--- lisp/init-ligatures.el
+++ lisp/init-ligatures.el
@@ -17,6 +17,13 @@
   :ensure t
   :config
   (ligature-set-ligatures 'prog-mode ligatures-jet-brains-mono)
+  (ligature-set-ligatures 'text-mode ligatures-jet-brains-mono)
+  (ligature-set-ligatures 'conf-mode ligatures-jet-brains-mono)
+  (ligature-set-ligatures 'vterm-mode ligatures-jet-brains-mono)
+  (ligature-set-ligatures 'compilation-mode ligatures-jet-brains-mono)
+  (ligature-set-ligatures 'dired-mode ligatures-jet-brains-mono)
+  (ligature-set-ligatures 'fundamental-mode ligatures-jet-brains-mono)
+
   (global-ligature-mode t)
   )
 
---
+++ lisp/init-surround.el
@@ -0,0 +1,11 @@
+;;; init-surround.el --- Surround -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package surround
+  :ensure t
+  :bind-keymap ("C-c s" . surround-keymap))
+
+(provide 'init-surround)
+
+;;; init-surround.el ends here

072f2cfa63dd64186ff527944a433db92274c626
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2026-03-27 15:00:53 +0500

    fix: Conditionally fullscreen, since it sometime's didn't work on X11

--- early-init.el
+++ early-init.el
@@ -1,3 +1,7 @@
+;;; early-init.el --- Mostly set variables and initial settings here before we get to init.el -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
 (setq pgtk-wait-for-event-timeout 0) ;; Don't delay waiting for GTK events after making changes to GUI
 (setenv "GDK_BACKEND" "x11")
 
@@ -5,8 +9,13 @@
 
 (setenv "SHELL" "/usr/bin/zsh")
 
-;; Start Emacs in maximized mode
-(toggle-frame-maximized)
-;; and hide title bar
-(add-hook 'window-size-change-functions
-  #'frame-hide-title-bar-when-maximized)
+(when (and (getenv "XDG_SESSION_TYPE")
+        (string= (getenv "XDG_SESSION_TYPE") "wayland"))
+  
+  ;; Start Emacs in maximized mode
+  (toggle-frame-maximized)
+  ;; and hide title bar
+  (add-hook 'window-size-change-functions
+    #'frame-hide-title-bar-when-maximized))
+
+;;; early-init.el ends here
